0c3c6a093f0f76cdff145bcb0d07287558c98926
stalfox <stalf0x@yandex.ru>
2019-02-18 17:31:20 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -24,6 +24,8 @@ public class UserMealsUtil {
 //        .toLocalTime();
     }
 
+
+
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with correctly exceeded field
         return null;
